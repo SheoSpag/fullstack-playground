b8baebdd0e4fb92c2f05f3d4d5b05d19022d047c
Giovanni Spagnuolo <spagnuologiovanni03@gmail.com>
2025-08-08 00:42:00 +0200

    Mejoras significativas, agregamos el menu, seguimos mejorando las css skils, cuesta encontrar inspiracion pero poco a poco vamos agarrandole la mano tanto al diseno como a react en general

---
+++ src/components/MenuItem.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./MenuItem.css";
+
+function MenuItem(){
+
+    return (
+        <div className="item_container">
+            <div className="item_header">
+                <h2 className="item_header_title">FIRE SPISY</h2>
+                <img src="../../public/logo.png" alt="" className="item_header_img" />
+            </div>
+            <div className="item_img_container">
+                <img className="item_img" src="../../public/empanada.png" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default MenuItem;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import "./Home.css";
 
 import NavBar from "../components/NavBar";
+import MenuItem from "../components/MenuItem";
 
 
 function Home(){
@@ -12,12 +13,20 @@ function Home(){
                 <NavBar />
                 
                 <main className="home_main">
-                    <h1 className='home_main_title'>THE F<span className="main_title_span">O</span>OD TRUCK !</h1>
+                    <h1 className='home_main_title'>THE FOOD TRUCK !</h1>
                     <img src="../../public/empanada.png" alt="" className="main_img" />
                 </main>
 
                 <div className="home_menu">
-
+                    <h2 className="home_menu_title">THE F<span className="menu_title_span">O</span>OD TRUCK MENU</h2>
+                    <div className="home_menu_items">    
+                        <MenuItem />
+                        <MenuItem />
+                        <MenuItem />
+                        <MenuItem />
+                        <MenuItem />
+                        <MenuItem />
+                    </div>
                 </div>
             </div>
         </>
